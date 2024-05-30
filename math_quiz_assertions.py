4271457ef3662ec551b92c3b1b714d6f8bb7b4d9
Matt Helmer <mahelmer28@gmail.com>
2024-05-30 14:10:18 -0700

    clean up main

--- math_quiz_assertions.py
+++ math_quiz_assertions.py
@@ -25,6 +25,7 @@ class GenerateAnswerChoices(dspy.Signature):
     number_of_choices = dspy.InputField()
     answer_choices = dspy.OutputField(desc='JSON key-value pairs')
 
+
 class QuizAnswerGenerator(dspy.Module):
     """Generate 'n' answer choices to a question using a JSON signature.
     """
@@ -33,7 +34,11 @@ class QuizAnswerGenerator(dspy.Module):
         self.generate_choices = dspy.ChainOfThought(GenerateAnswerChoices)
 
     def forward(self, question, answer):
-        choices = self.generate_choices(question=question, correct_answer=answer, number_of_choices='4').answer_choices
+        choices = self.generate_choices(question=question,
+                                        correct_answer=answer,
+                                        number_of_choices='4'
+                                        ).answer_choices
+
         return dspy.Prediction(choices=choices)
 
 
@@ -46,21 +51,52 @@ class QuizAnswerGeneratorWithAssertions(dspy.Module):
         self.generate_choices = dspy.ChainOfThought(GenerateAnswerChoices)
 
     def forward(self, question, answer):
-        choice_string = self.generate_choices(question=question, correct_answer=answer, number_of_choices='4').answer_choices
-        dspy.Suggest(format_checker(choice_string), "The format of the answer choices should be in JSON format. Please revise accordingly.", target_module=GenerateAnswerChoices)
-        dspy.Suggest(is_correct_answer_included(answer, choice_string), "The answer choices do not include the correct answer to the question. Please revise accordingly.", target_module=GenerateAnswerChoices)
-        plausibility_question = "Are the distractors in the answer choices plausible and not easily identifiable as incorrect?"
-        plausibility_assessment = dspy.Predict(AssessQuizChoices)(question=question, answer_choices=choice_string, assessment_question=plausibility_question)
-        dspy.Suggest(is_plausibility_yes(plausibility_assessment.assessment_answer),
-                     "The answer choices are not plausible distractors or are too easily identifiable as incorrect. Please revise to provide more challenging and plausible distractors.", target_module=GenerateAnswerChoices)
-        return dspy.Prediction(choices=choice_string)
+        choice_string = self.generate_choices(question=question,
+                                              correct_answer=answer,
+                                              number_of_choices='4'
+                                              ).answer_choices
+
+        format_suggestion = ("The answer choices should be in JSON format. "
+                            "Please revise accordingly.")
+        dspy.Suggest(format_checker(choice_string), format_suggestion,
+                    target_module=GenerateAnswerChoices)
+
+        answer_suggestion = ("The answer choices do not include the correct "
+                            "answer to the question. Please revise "
+                            "accordingly.")
+        dspy.Suggest(is_correct_answer_included(answer, choice_string),
+                    answer_suggestion, target_module=GenerateAnswerChoices)
+
+        plausibility_question = ("Are the distractors in the answer choices "
+                                 "plausible and not easily identifiable as "
+                                 "incorrect?")
+        plausibility_assessment = dspy.Predict(AssessQuizChoices)(
+                question=question,
+                answer_choices=choice_string,
+                assessment_question=plausibility_question,
+                )
+        plausibility_suggestion = ("The answer choices are not plausible "
+                                   "distractors or are too easily identifiable"
+                                   " as incorrect. Please revise to provide "
+                                   "more challenging and plausible "
+                                   "distractors.")
+        dspy.Suggest(
+                is_plausibility_yes(plausibility_assessment.assessment_answer),
+                plausibility_suggestion,
+                target_module=GenerateAnswerChoices
+        )
+        result = dspy.Prediction(choices=choice_string)
+        return result
 
 
 ### EVALUATION METRICS ###
 def format_checker(choice_string):
     try:
         choices = json.loads(choice_string)
-        if isinstance(choices, dict) and all(isinstance(key, str) and isinstance(value, str) for key, value in choices.items()):
+        if isinstance(choices, dict) and all(
+                isinstance(key, str) and isinstance(value, str)
+                for key, value in choices.items()
+        ):
             return True
     except json.JSONDecodeError:
         return False
@@ -104,9 +140,18 @@ def is_correct_metric(gold, pred, trace=None):
 
 def plausibility_metric(gold, pred, trace=None):
     question, generated_choices = gold.question, pred.choices
-    plausibility_question = "Are the distractors in the answer choices plausible and not easily identifiable as incorrect?"
-    plausibility_assessment = dspy.Predict(AssessQuizChoices)(question=question,answer_choices=generated_choices, assessment_question=plausibility_question)
-    plausibility_result = plausibility_assessment.assessment_answer.split()[0].lower() == 'yes'
+    plausibility_question = ("Are the distractors in the answer choices "
+                             "plausible and not easily identifiable as "
+                             "incorrect?")
+
+    plausibility_assessment = dspy.Predict(AssessQuizChoices)(
+                                    question=question,
+                                    answer_choices=generated_choices,
+                                    assessment_question=plausibility_question
+                                    )
+
+    first_word = plausibility_assessment.assessment_answer.split()[0].lower()
+    plausibility_result = first_word == 'yes'
     score = plausibility_result
     return score
 
@@ -115,19 +160,32 @@ def overall_metric(gold, pred, trace=None):
     question, correct_answer, generated_choices = gold.question, gold.answer, pred.choices
     format_valid = format_checker(generated_choices)
     correct_included = is_correct_answer_included(correct_answer, generated_choices)
-    plausibility_question = "Are the distractors in the answer choices plausible and not easily identifiable as incorrect?"
-    plausibility_assessment = dspy.Predict(AssessQuizChoices)(question=question, answer_choices=generated_choices, assessment_question=plausibility_question)
-    plausibility_result = plausibility_assessment.assessment_answer.split()[0].lower() == 'yes'
-    score = (format_valid + correct_included + plausibility_result) / 3.0 if correct_included and format_valid else 0
+    plausibility_question = ("Are the distractors in the answer choices "
+                             "plausible and not easily identifiable as "
+                             "incorrect?")
+    plausibility_assessment = dspy.Predict(AssessQuizChoices)(
+                                question=question,
+                                answer_choices=generated_choices,
+                                assessment_question=plausibility_question
+                                )
+
+    first_word = plausibility_assessment.assessment_answer.split()[0].lower()
+    plausibility_result = first_word == 'yes'
+    if correct_included and format_valid:
+        score = (format_valid + correct_included + plausibility_result) / 3.0
+    else:
+        score = 0
     return score
 
 
 def main():
     # set up the LM
-    turbo = dspy.OpenAI(model='gpt-3.5-turbo-instruct', max_tokens=250, api_key=OPENAI_API_KEY)
-    dspy.settings.configure(lm=turbo, trace=[])
+    lm = dspy.OpenAI(
+            model='gpt-4o', max_tokens=1000, api_key=OPENAI_API_KEY
+            )
+    dspy.settings.configure(lm=lm, trace=[])
 
-    # load math questions from dataset
+    # load Grade School Math questions dataset
     gsm8k = GSM8K()
     trainset = [x.with_inputs('question', 'answer') for x in gsm8k.train]
     devset = [x.with_inputs('question', 'answer') for x in gsm8k.dev]
@@ -136,102 +194,88 @@ def main():
     quiz_generator = QuizAnswerGenerator()
 
     # set up a quiz generator with assertions
-    quiz_generator_with_assertions = assert_transform_module(QuizAnswerGeneratorWithAssertions().map_named_predictors(Retry), backtrack_handler)
+    quiz_generator_with_assertions = assert_transform_module(
+            QuizAnswerGeneratorWithAssertions().map_named_predictors(Retry),
+            backtrack_handler
+    )
 
-    metrics = [
-            format_valid_metric,
-            is_correct_metric,
-            plausibility_metric,
-            overall_metric,
-            ]
+    metrics = [format_valid_metric, is_correct_metric, plausibility_metric,
+               overall_metric]
 
     # each of 3 test examples below will:
-    # generate few-shot demonstrations and conduct a random search over the
-    # candidates to output the best compiled program
+    # - generate few-shot demonstrations and
+    # - conduct random search over candidates to output best compiled program
 
+    compiled_programs = []
 
-    # 1) test without assertions
-    teleprompter = BootstrapFewShotWithRandomSearch(metric=overall_metric,
-                                                    max_bootstrapped_demos=2,
-                                                    num_candidate_programs=6)
+    # 1) compiled, but no Assertions
+    teleprompter = BootstrapFewShotWithRandomSearch(
+            metric=overall_metric,
+            max_bootstrapped_demos=2,
+            num_candidate_programs=6
+            )
 
-    compiled_quiz_generator = teleprompter.compile(
+    compiled_quiz_gen = teleprompter.compile(
             student=quiz_generator,
             teacher=quiz_generator,
             trainset=trainset,
-            valset=devset[:100])
+            valset=devset[:100]
+            )
 
-    for metric in metrics:
-        evaluate = Evaluate(metric=metric,
-                            devset=devset,
-                            num_threads=1,
-                            display_progress=True,
-                            )
-        evaluate(compiled_quiz_generator)
-
+    compiled_programs.append(('No Assertions', compiled_quiz_gen))
 
     # 2) test_compilation_with_teacher_assertions
-    teleprompter = BootstrapFewShotWithRandomSearch(metric=overall_metric,
-                                                    max_bootstrapped_demos=2,
-                                                    num_candidate_programs=6)
+    teleprompter = BootstrapFewShotWithRandomSearch(
+            metric=overall_metric,
+            max_bootstrapped_demos=2,
+            num_candidate_programs=6
+            )
 
-    compiled_with_teacher_assertions_quiz_generator = teleprompter.compile(
+    compiled_quiz_gen_teacher_assertions = teleprompter.compile(
             student=quiz_generator,
             teacher=quiz_generator_with_assertions,
             trainset=trainset,
-            valset=devset[:100])
+            valset=devset[:100]
+            )
 
-    for metric in metrics:
-        evaluate = Evaluate(metric=metric,
-                            devset=devset,
-                            num_threads=1,
-                            display_progress=True,
-                            )
-        evaluate(compiled_with_teacher_assertions_quiz_generator)
 
+    compiled_programs.append(('Teacher Assertions', compiled_quiz_gen_teacher_assertions))
 
     # 3) test compilation with both student and teacher assertions
-    teleprompter = BootstrapFewShotWithRandomSearch(metric=overall_metric,
-                                                    max_bootstrapped_demos=2,
-                                                    num_candidate_programs=6)
+    teleprompter = BootstrapFewShotWithRandomSearch(
+            metric=overall_metric,
+            max_bootstrapped_demos=2,
+            num_candidate_programs=6
+            )
 
-    compiled_with_assertions_quiz_generator = teleprompter.compile(
-            student=quiz_generator,
+    compiled_quiz_gen_assertions = teleprompter.compile(
+            student=quiz_generator_with_assertions,
             teacher=quiz_generator_with_assertions,
             trainset=trainset,
-            valset=devset[:100])
+            valset=devset[:100]
+            )
 
-    for metric in metrics:
-        evaluate = Evaluate(metric=metric,
-                            devset=devset,
-                            num_threads=1,
-                            display_progress=True,
-                            )
-        evaluate(compiled_with_assertions_quiz_generator)
+    compiled_programs.append(('Teacher+Student Assertions', compiled_quiz_gen_assertions))
 
-    # load a random example for comparison across 3 compiled quiz generators
+    # load a random example to compare across 3 compiled programs
     import random
     example = random.choice(devset)
 
-    print(f'Quiz Question: ', example.question)
-    print(f'Quiz Answer: ', example.answer)
+    for compiled_program in compiled_programs:
+        for metric in metrics:
+            evaluate = Evaluate(metric=metric, devset=devset, num_threads=1,
+                                display_progress=True)
+            evaluate(compiled_program[1])
 
-    quiz_choices_no_assertions = compiled_quiz_generator(
-            question=example.question,
-            answer=example.answer,
-            )
-    quiz_choices_teacher_assertions = compiled_with_teacher_assertions_quiz_generator(
-            question=example.question,
-            answer=example.answer,
-            )
-    quiz_choices_assertions = compiled_with_assertions_quiz_generator(
-            question=example.question,
-            answer=example.answer,
-            )
 
-    print(f'Generated Quiz Choices (no assertions): ', quiz_choices_no_assertions.choices)
-    print(f'Generated Quiz Choices (teacher assertions): ', quiz_choices_teacher_assertions.choices)
-    print(f'Generated Quiz Choices (teacher and student assertions): ', quiz_choices_assertions.choices)
+    print(f'Random Quiz Question: ', example.question)
+    print(f'Answer: ', example.answer)
+
+    for compiled_program in compiled_programs:
+        cp_str, compiled_program = compiled_program
+        choices = compiled_program(question=example.question,
+                                   answer=example.answer)
+        print(f'Answer Choices by {cp_str}: ', choices.choices)
 
 
 if __name__ == '__main__':
